c3658a37c5e91234f6deab64c66fd62fb46556aa
ilsetse <ilsetse@gmail.com>
2018-02-27 22:46:23 +0200

    added templates

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,24 @@
 import socket
-from flask import Flask
+import os
+from datetime import datetime
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    
-    return "Hello World! Greetings from "+socket.gethostname()+ " at " "\n"
+    try:
+      pod_hostname = socket.gethostname()
+      cur_time = str(datetime.now())
+      #oc_log = os.env('OPENSHIFT_DATA_DIR')
+      return render_template('hello.html',
+                          pod_hostname=pod_hostname,
+                          cur_time=cur_time)
+                          #log=oc_log) 
+    except:
+      return "Hello World! Greetings from "+socket.gethostname() + "\n"
+      pass
 
 
 if __name__ == "__main__":
-    application.run()
+    application.run(debug=True)
