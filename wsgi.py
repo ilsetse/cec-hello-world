2e47e12e90b9ca3661936b853c1c9940612cc4eb
ilsetse <ilsetse@gmail.com>
2018-02-28 02:13:58 +0200

    added logger

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 import socket
 import os
 import sys
-from datetime import datetime
+import time
+import logging
+
 from flask import Flask, render_template
 
 application = Flask(__name__)
@@ -10,9 +12,33 @@ application = Flask(__name__)
 def hello():
     try:
       pod_hostname = socket.gethostname()
-      cur_time = str(datetime.now())
-      #oc_log = os.env('OPENSHIFT_DATA_DIR')
+      cur_time = time.ctime()
+     
+      logging.basicConfig(filename='example.log',level=logging.DEBUG)
+      logging.info(pod_hostname + ' ' + cur_time + '\n')
+      #logger=logging.getLogger(__name__)
+      #logger.setLevel(loggin.NOTICE)
+
+      # file handler
+      #handler = logging.FileHandler(os.path.join(os.environ.get('OPENSHIFT_DATA_DIR','app.log')))
+      #handler.setLevel(logging.NOTICE)
+
+      # create logging format
+      #formatter = logging.Formatter('%(asctime)s')
+      #handler.setFormatter(formatter)
+
+      # add handlers to logger
+      #logger.addHandler(handler)
+      
+      
+      #logging.basicConfig(format=FORMAT,level=NOTICE)
+      #handler = logging.FileHandler(logfile_name)
+      #handler.setLevel(logging.notice)
+
+
+      #print($OPENSHIFT_DATA_DIR)
       oc_log = sys.executable
+
       return render_template('hello.html',
                           pod_hostname=pod_hostname,
                           cur_time=cur_time,
