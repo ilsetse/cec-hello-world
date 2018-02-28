9f26eb5c8a1d9c0abe588960365154f52de476f5
ilsetse <ilsetse@gmail.com>
2018-02-28 03:49:32 +0200

    added templates

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 import socket
 import os
+import sys
 import time
 import logging
-import sys
 
 from flask import Flask, render_template
 
@@ -13,17 +13,20 @@ def hello():
     try:
       pod_hostname = socket.gethostname()
       cur_time = time.ctime()
+    
       pvc1 = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR'),'app.log')
       logging.basicConfig(filename=pvc1,level=logging.DEBUG)
       logging.info(pod_hostname + ' ' + cur_time + '\n')
+
       ex  = open(pvc1, 'r')
       oc_log = ex.readline()
+
       return render_template('hello.html',
                           pod_hostname=pod_hostname,
-                          cur_time=cur_time)
-                          log=oc_log)
+                          cur_time=cur_time,
+                          log=oc_log) 
     except:
-      return "Hello World! Greetings from " + socket.gethostname() + "\n"
+      return "Hello World! Greetings from "+socket.gethostname() + "\n"
       pass
 
 
